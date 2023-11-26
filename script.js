b69579b9a19a99c784cb8e090430e68c34e9a751
eduardofgomes <eduardofranciscogomes01@gmail.com>
2023-11-26 17:09:49 -0300

    Hover

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-const button = document.querySelector("theme-button")
+const button = document.querySelector("#theme-button")
+const sectionDark = document.querySelector(".bg-black")
 
 
 button.addEventListener("click", function(e) {
     e.preventDefault()
 
+    console.log("button clicked")
 
+    sectionDark.remove("bg-black")
 })
